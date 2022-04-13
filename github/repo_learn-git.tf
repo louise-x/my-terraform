df0b810900de2bd24098eb7c70ad86080a4be2c4
louis <k2k@1.comwq>
2022-04-13 08:06:44 +0000

    import my repo

--- github/repo_learn-git.tf
+++ github/repo_learn-git.tf
@@ -1,2 +1,12 @@
 resource "github_repository" "learn-git" {
+   allow_merge_commit = true
+   allow_rebase_merge = true
+   allow_squash_merge = true
+   description = "this is playground of learning git"
+   name = "learn-git"
+   visibility = "public"
+   has_downloads          = true
+   has_issues             = true
+   has_projects           = true
+   has_wiki               = true
 }
